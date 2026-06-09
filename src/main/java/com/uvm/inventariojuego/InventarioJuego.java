52d7d6414a86a01f3f1386f25d13e319fd18609b
jonra <jonra@192.168.1.76>
2026-06-09 16:36:01 -0600

    boceto main

--- src/main/java/com/uvm/inventariojuego/InventarioJuego.java
+++ src/main/java/com/uvm/inventariojuego/InventarioJuego.java
@@ -1,12 +1,42 @@
-package com.uvm.inventariojuego;
+import java.util.Scanner;
 
-/**
- *
- * @author nimev
- */
-public class InventarioJuego {
+public class Inventariojuego{
+  public static void main(String args[]){
+ 
+   Scanner teclado = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+   int botonSalir = 0;
+   int opcion = 0;
+
+   while (botonSalir ==0){
+
+    try{
+    
+      System.out.println("\n=== INVENTARIO DE TU PERSONAJE ===");
+      System.out.println("1. Ver Espada");
+      System.out.println("2. Ver Pocion de vida");
+      System.out.println("3. Salir del juego");
+      System.out.println("Elige una opcion: ");
+
+      opcion = teclado.nextInt();
+
+      if (opcion ==1){
+         System.out.println("-> [Espada de Hierro]: Ataque +15. Ideal para matar dragones.");
+      }
+       else if (opcion ==2){
+            System.out.println("-> [Pocion Roja]: Recupera 50 puntos de salud.");
+      }
+       else if (opcion ==3){
+         System.out.println("-> Cerrando mochila... ¡Buena suerte!");
+         botonSalir = 1;
+      }
+       else{
+          System.out.println("-> Opción invalida. Intenta con 1, 2 o 3.");
+      }
+
+  }
+
+  catch (Exception e){
+    System.out.println("-> Error: ¡Debes ingresar un numero entero!");
+    teclado.next();
+
