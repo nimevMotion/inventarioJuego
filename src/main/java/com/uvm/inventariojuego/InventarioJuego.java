a45e1c5b523501a91e36e6ba5a85099fe730aeeb
nimevMotion <54727989+nimevMotion@users.noreply.github.com>
2026-06-09 19:20:18 -0600

    Limpieza en clase main

--- src/main/java/com/uvm/inventariojuego/InventarioJuego.java
+++ src/main/java/com/uvm/inventariojuego/InventarioJuego.java
@@ -1,42 +1,40 @@
+package com.uvm.inventariojuego;
+
 import java.util.Scanner;
 
-public class Inventariojuego{
-  public static void main(String args[]){
- 
-   Scanner teclado = new Scanner(System.in);
+public class InventarioJuego {
 
-   int botonSalir = 0;
-   int opcion = 0;
+    public static void main(String args[]) {
 
-   while (botonSalir ==0){
+        Scanner teclado = new Scanner(System.in);
 
-    try{
-    
-      System.out.println("\n=== INVENTARIO DE TU PERSONAJE ===");
-      System.out.println("1. Ver Espada");
-      System.out.println("2. Ver Pocion de vida");
-      System.out.println("3. Salir del juego");
-      System.out.println("Elige una opcion: ");
+        int botonSalir = 0;
+        int opcion = 0;
 
-      opcion = teclado.nextInt();
+        while (botonSalir == 0) {
 
-      if (opcion ==1){
-         System.out.println("-> [Espada de Hierro]: Ataque +15. Ideal para matar dragones.");
-      }
-       else if (opcion ==2){
-            System.out.println("-> [Pocion Roja]: Recupera 50 puntos de salud.");
-      }
-       else if (opcion ==3){
-         System.out.println("-> Cerrando mochila... ¡Buena suerte!");
-         botonSalir = 1;
-      }
-       else{
-          System.out.println("-> Opción invalida. Intenta con 1, 2 o 3.");
-      }
+            try {
+                System.out.println("\n=== INVENTARIO DE TU PERSONAJE ===");
+                System.out.println("2. Ver Pocion de vida");
+                System.out.println("3. Salir del juego");
+                System.out.println("Elige una opcion: ");
 
-  }
+                opcion = teclado.nextInt();
 
-  catch (Exception e){
-    System.out.println("-> Error: ¡Debes ingresar un numero entero!");
-    teclado.next();
-
+                if (opcion == 1) {
+                    System.out.println("-> [Espada de Hierro]: Ataque +15. Ideal para matar dragones.");
+                } else if (opcion == 2) {
+                    System.out.println("-> [Pocion Roja]: Recupera 50 puntos de salud.");
+                } else if (opcion == 3) {
+                    System.out.println("-> Cerrando mochila... ¡Buena suerte!");
+                    botonSalir = 1;
+                } else {
+                    System.out.println("-> Opción invalida. Intenta con 1, 2 o 3.");
+                }
+            } catch (Exception e) {
+                System.out.println("-> Error: ¡Debes ingresar un numero entero!");
+                teclado.next();
+            }
+        }
+    }
+}
